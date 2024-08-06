0a607d3bdcf352d2a95c18d8488b81f416075a7c
EdwardoMelo <ti@dse.com.br>
2024-08-06 15:14:02 -0300

    new Resume

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -3,18 +3,30 @@ import { BsGithub, BsLinkedin} from 'react-icons/bs'
 import { AiFillFileText } from "react-icons/ai";
 const SocialMedia = () => {
   return (
-    <div className='app__social'>
-        <div>
-            <a target='blank' href="https://github.com/EdwardoMelo"><BsGithub /></a>
-        </div>
-        <div>
-            <a target='blank' href="https://www.linkedin.com/in/eduardo-melo-4025bb202/"><BsLinkedin /> </a>
-        </div>
-        <div>
-            <a target='blank' href="https://drive.google.com/file/d/1Yxa5LMbO3GohwDWcdMMUsFQK3xuEiSuM/view?usp=drive_link"><AiFillFileText /></a>
-        </div>
+    <div className="app__social">
+      <div>
+        <a target="blank" href="https://github.com/EdwardoMelo">
+          <BsGithub />
+        </a>
+      </div>
+      <div>
+        <a
+          target="blank"
+          href="https://www.linkedin.com/in/eduardo-melo-4025bb202/"
+        >
+          <BsLinkedin />{" "}
+        </a>
+      </div>
+      <div>
+        <a
+          target="blank"
+          href="https://drive.google.com/file/d/1IzoVrjidVA1aQE8FXuMLMcOnMK4tYcUD/view?usp=drive_link"
+        >
+          <AiFillFileText />
+        </a>
+      </div>
     </div>
-  )
+  );
 }
 
 export default SocialMedia
